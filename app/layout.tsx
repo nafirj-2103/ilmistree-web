2d636d033cbfe2dda520daad775ae780dcf1f245
Nafeel Qamar <nafix2103@gmail.com>
2026-04-14 22:04:31 +0500

    mobile drawer dual nav added

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,18 +13,6 @@ export default function RootLayout({
 }) { 
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Godber:wght@400;500;700&display=optional"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Milk+and+Honey&display=optional"
-          rel="stylesheet"
-        />
-      </head>
       <body className="min-h-screen pt-[73px]">{children}</body>
     </html>
   )
